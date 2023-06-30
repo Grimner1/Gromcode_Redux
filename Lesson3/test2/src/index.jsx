8cdfbee15e2c3967f8c5ed880e78b98a956bffbf
Dmitry <ursuld@gmail.com>
2023-06-30 14:30:26 +0300

    L3 T2 (2)

--- Lesson3/test2/src/index.jsx
+++ Lesson3/test2/src/index.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App.jsx';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.scss';
+// import App from './App.jsx';
 
-const rootElement = document.querySelector('#root');
+// const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElement);
+// ReactDOM.render(<App />, rootElement);
+
+import store from "./store";
+import { increment, decrement } from "../../test1/src/counter.actions";
+
+console.log(store);
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+
+console.log(store.getState());
---
+++ Lesson3/test2/src/store.js
@@ -0,0 +1,36 @@
+import { createStore } from "redux";
+
+const INCREMENT = "COUNTER/INCREMENT";
+const DECREMENT = "COUNTER/DECREMENT";
+
+const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "COUNTER/INCREMENT":
+      return state + 1;
+
+    case "COUNTER/DECREMENT":
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
