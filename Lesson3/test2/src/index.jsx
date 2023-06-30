c7503dd754f9fe28ee9e1537cd4a7812eac76feb
Dmitry <ursuld@gmail.com>
2023-06-30 14:41:34 +0300

    4

--- Lesson3/test2/src/index.jsx
+++ Lesson3/test2/src/index.jsx
@@ -7,13 +7,13 @@
 
 // ReactDOM.render(<App />, rootElement);
 
-import store from "./counter";
-import { increment, decrement } from "./counter";
+// import store from "./counter";
+// import { increment, decrement } from "./counter";
 
-console.log(store);
+// console.log(store);
 
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
+// store.dispatch(increment());
+// store.dispatch(increment());
+// store.dispatch(decrement());
 
-console.log(store.getState());
+// console.log(store.getState());
