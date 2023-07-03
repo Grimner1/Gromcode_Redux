711c0b816f48c8dbd89d9f11823cb8115982248a
Dmitry <ursuld@gmail.com>
2023-07-03 12:08:16 +0300

    L3 T2_V2

---
+++ Lesson3/test2_v2/src/counter.js
@@ -0,0 +1,34 @@
+import { createStore } from "redux";
+
+const INCREMENT = "COUNTER/INCREMENT";
+const DECREMENT = "COUNTER/DECREMENT";
+
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return (state = state + 1);
+    case DECREMENT:
+      return (state = state - 1);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
