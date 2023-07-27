f7281b7e48c9434712520e3bb702cab4ab8daae7
Dmitry <ursuld@gmail.com>
2023-07-27 15:35:53 +0300

    5

--- Lesson4/test3/src/users/Pagination.jsx
+++ Lesson4/test3/src/users/Pagination.jsx
@@ -9,10 +9,12 @@ const Pagination = ({
 }) => {
   const isPrevPageAvailable = currentPage == 0;
 
-  const isNextPageAvailable = () => {
+  const isNextPageAvailablex = () => {
     const lastPageNumber = Math.floor(totalItems / itemsPerPage);
     return lastPageNumber == currentPage;
   };
+
+  const isNextPageAvailable = isNextPageAvailablex();
 
   return (
     <div className="pagination">
@@ -20,8 +22,8 @@ const Pagination = ({
         {!isPrevPageAvailable && "←"}
       </button>
       <span className="pagination__page">{currentPage + 1}</span>
-      <button className="btn" onClick={goNext} disabled={isNextPageAvailable()}>
-        {!isNextPageAvailable() && "→"}
+      <button className="btn" onClick={goNext} disabled={isNextPageAvailable}>
+        {!isNextPageAvailable && "→"}
       </button>
     </div>
   );
